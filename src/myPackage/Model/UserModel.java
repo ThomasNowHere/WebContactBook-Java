327f47e331e09f5d9fdd0b0df3b510aa130426d1
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2016-08-11 16:08:41 +0200

    user model start

---
+++ src/myPackage/Model/UserModel.java
@@ -0,0 +1,24 @@
+package myPackage.Model;
+
+public class UserModel {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
