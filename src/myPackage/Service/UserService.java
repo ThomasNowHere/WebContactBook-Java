214eb7db6dc391d272b91b71dd3293ae1d0cde6c
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2016-08-18 15:33:07 +0200

    register method

--- src/myPackage/Service/UserService.java
+++ src/myPackage/Service/UserService.java
@@ -10,7 +10,17 @@ public class UserService {
     
     public static int register(UserModel user) {
         int status = 0;
-        // TODO implement
+        try {
+            Connection con = Database.getConnection();
+            PreparedStatement ps = con.prepareStatement("insert into users(username,password,email) values(?,?,?)");
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getEmail());
+            status = ps.executeUpdate();
+            con.close();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
         return status;
     }
     
