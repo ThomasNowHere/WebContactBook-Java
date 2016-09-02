e131c6c73ab6e37a55423d8829550688ae69e394
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2016-09-02 22:38:11 +0200

    address model getters

--- src/myPackage/Model/AddressModel.java
+++ src/myPackage/Model/AddressModel.java
@@ -14,4 +14,19 @@ public class AddressModel {
     
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+    
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    
+    public String getMobile() { return mobile; }
+    public void setMobile(String mobile) { this.mobile = mobile; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
 }
