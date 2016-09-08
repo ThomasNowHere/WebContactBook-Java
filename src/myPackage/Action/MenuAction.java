4e9462447f63e379125a6a09d310b9120a5f3951
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2016-09-08 19:52:33 +0200

    menu action

---
+++ src/myPackage/Action/MenuAction.java
@@ -0,0 +1,17 @@
+package myPackage.Action;
+
+public class MenuAction {
+     
+	public String homeMenu(){
+		return "home";
+	}
+	public String addMenu(){
+		return "add";
+	}
+	public String searchMenu(){
+		return "search";
+	}
+	public String changeMenu(){
+		return "change";
+	}
+}
