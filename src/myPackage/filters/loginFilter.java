76681a17b12f5aa36949b3218b100799fafd3262
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2016-10-16 15:44:19 +0200

    filter fix

--- src/myPackage/filters/loginFilter.java
+++ src/myPackage/filters/loginFilter.java
@@ -1,26 +1,55 @@
 package myPackage.filters;
 
 import java.io.IOException;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+/**
+ * Servlet Filter implementation class loginFilter
+ */
 public class loginFilter implements Filter {
-    
-    public void init(FilterConfig config) throws ServletException {}
-    
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
-            throws IOException, ServletException {
-        
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        HttpSession session = req.getSession(false);
-        
-        if (session == null || session.getAttribute("userId") == null) {
-            res.sendRedirect(req.getContextPath() + "/main/login.jsp");
-        } else {
-            chain.doFilter(request, response);
-        }
+
+    /**
+     * Default constructor. 
+     */
+    public loginFilter() {
+        // TODO Auto-generated constructor stub
     }
-    
-    public void destroy() {}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession(false);
+		
+		if(session != null && session.getAttribute("uname") != null || req.getRequestURI().indexOf("/main") >= 0){
+			chain.doFilter(request, response);
+		}else{
+			res.sendRedirect(req.getContextPath()+"/main/login.jsp");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
 }
