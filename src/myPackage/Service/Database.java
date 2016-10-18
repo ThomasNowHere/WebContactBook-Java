f05db3fdde1ea6e29784f0f1b4e35b44de14dd5f
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2016-10-18 19:22:08 +0200

    db class update

--- src/myPackage/Service/Database.java
+++ src/myPackage/Service/Database.java
@@ -1,18 +1,29 @@
 package myPackage.Service;
-
-import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Connection;
+
 
 public class Database {
-    
-    public static Connection getConnection() {
-        Connection con = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressone", "root", "");
-        } catch (Exception e) {
-            System.out.println("error: " + e);
-        }
-        return con;
-    }
+
+	
+	public static Connection getConnection(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost/addressone", "root", "");
+			return con;
+			
+		}catch(Exception ex){
+			System.out.println(ex.getMessage());
+			return null;
+		}
+	}
+	
+	public static void close(Connection con){
+		try{
+			con.close();
+		}catch (Exception ex){
+			System.out.println(ex.getMessage());
+		}
+	}
+	
 }
